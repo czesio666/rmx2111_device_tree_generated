bdb1fac78648f6d95a5189d51c8c77048a5c1728
czesio666 <73993043+czesio666@users.noreply.github.com>
2026-05-18 22:55:54 +0200

    Update device.mk
    Display, touch fix

--- device.mk
+++ device.mk
@@ -6,3 +6,16 @@
 #
 
 LOCAL_PATH := device/realme/RMX2111
+
+# Display
+TARGET_USES_HWC2 := true
+TARGET_FORCE_HWC_FOR_GPU_CLIENT_COMPOSITION := true
+
+# Panel & Backlight
+DEVICE_SPECIFIC_TW := true
+
+# Touch fixes
+TARGET_TOUCHSCREEN := focaltech
+
+# Power management
+TARGET_USES_ION := true
